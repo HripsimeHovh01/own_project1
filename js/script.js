c1fbc0f4076bffa886ff6ea5bc912d5bc4714da2
Hripsime Hovhannisyan <hripsime.hovhan01@gmail.com>
2024-07-17 14:35:53 +0400

    we added status for user wich visited country count will be more than 0 and we edited answers for loop

--- js/script.js
+++ js/script.js
@@ -3,23 +3,34 @@
 const countOfCountry = +prompt("How many countries have you visited?")
 
 const obj = {
-count: countOfCountry,
-continent: {},
-ocean: {},
-religion: [],
-private: false
+    count: countOfCountry,
+    continent: {},
+    ocean: {},
+    religion: [],
+    status: null,
+    private: false
 };
 
-const x = prompt("Which country did you visit first?");
-const y = prompt("Which country do you like the most?");
-const z = +prompt("How would you rate your last visited country?");
-const a = prompt("Which country did you visit first?");
-const b = prompt("Which country do you like the most?");
-const c = +prompt("How would you rate your last visited country?");
+for (let i = 0; i < 1; i++) {
+    const firstCountry = prompt("Which country did you visit first?");
+    const rateCountry = +prompt(`How would you rate your visit to ${firstCountry}?`);
+    const likeCountry = prompt("Which country do you like the most?");
 
-obj.continent[x] = y;
-obj.continent[y] = z;
-obj.continent[a] = b;
-obj.continent[b] =c;
-
+if (firstCountry != null && likeCountry != null && rateCountry != null && firstCountry != "" && likeCountry != "" && rateCountry != "" && firstCountry.length < 30) {
+    obj.continent[firstCountry] = rateCountry;
+console.log("Done");
+    } else {
+        console.log("Canceled");
+        i--;
+    }
 console.log(obj);
+}
+
+if (obj.count < 2){
+obj.status = "You aren't travel lover";
+} else if 
+    (obj.count >= 10) {
+    obj.status = "You are travel lover"
+    } else {
+        console.log("It happened problem ");
+    }
