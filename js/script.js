341773a91b680ebd75858508447d92476ec45da7
Hripsime Hovhannisyan <hripsime.hovhan01@gmail.com>
2024-07-29 12:55:27 +0400

    In our project we added 2 functions with returns

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 "use script"
 
-const countOfCountry = +prompt("How many countries have you visited?")
+let countOfCountry;
 
+function init() {
+ countOfCountry = +prompt("How many countries have you visited?");
+
+while (countOfCountry == "" || countOfCountry == null || isNaN(countOfCountry)) {
+    countOfCountry = +prompt("How many countries have you visited?");
+}
+
+}
+init()
 const obj = {
     count: countOfCountry,
     continent: {},
@@ -10,27 +19,38 @@ const obj = {
     status: null,
     private: false
 };
-
-for (let i = 0; i < 1; i++) {
-    const firstCountry = prompt("Which country did you visit first?");
-    const rateCountry = +prompt(`How would you rate your visit to ${firstCountry}?`);
-    const likeCountry = prompt("Which country do you like the most?");
-
-if (firstCountry != null && likeCountry != null && rateCountry != null && firstCountry != "" && likeCountry != "" && rateCountry != "" && firstCountry.length < 30) {
-    obj.continent[firstCountry] = rateCountry;
-console.log("Done");
-    } else {
-        console.log("Canceled");
-        i--;
+  function rememberAndCreateUserCountry() {
+    for (let i = 0; i < 1; i++) {
+        const firstCountry = prompt("Which country did you visit first?");
+        const rateCountry = +prompt(`How would you rate your visit to ${firstCountry}?`);
+        const likeCountry = prompt("Which country do you like?");
+    
+    if (firstCountry != null && likeCountry != null && rateCountry != null && firstCountry != "" && likeCountry != "" && rateCountry != "" && firstCountry.length < 30) {
+        obj.continent[firstCountry] = rateCountry;
+    console.log("Done");
+        } else {
+            console.log("Canceled");
+            i--;
+        }
+    console.log(obj);
     }
-console.log(obj);
+  }
+function detectUserPersonalLevel() {
+    if (obj.count < 2){
+        obj.status = "You aren't travel lover";
+        } else if 
+            (obj.count >= 10) {
+            obj.status = "You are travel lover"
+            } else {
+                console.log("It happened problem ");
+            }  
 }
 
-if (obj.count < 2){
-obj.status = "You aren't travel lover";
-} else if 
-    (obj.count >= 10) {
-    obj.status = "You are travel lover"
-    } else {
-        console.log("It happened problem ");
-    }
+
+function checkObjPrivateStatus(state) {
+    state ? console.log("Sorry but obj is not visible"): console.log(obj);
+    
+}
+rememberAndCreateUserCountry();
+detectUserPersonalLevel();
+checkObjPrivateStatus(obj.private);
