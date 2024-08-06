4b094cdb611c4deeb673449e2c19218d877902dd
Hripsime Hovhannisyan <hripsime.hovhan01@gmail.com>
2024-08-06 16:01:50 +0400

    changed file and all application we wrapped in object and used  THIS

--- js/script.js
+++ js/script.js
@@ -1,65 +1,64 @@
 "use script"
 
-let countOfCountry;
-
-function init() {
- countOfCountry = +prompt("How many countries have you visited?");
-
-while (countOfCountry == "" || countOfCountry == null || isNaN(countOfCountry)) {
-    countOfCountry = +prompt("How many countries have you visited?");
-}
-
-}
-init()
-const obj = {
-    count: countOfCountry,
+const app = {
+    count: 0,
     continent: {},
     ocean: {},
     religion: [],
     status: null,
-    private: false
-};
-  function rememberAndCreateUserCountry() {
-    for (let i = 0; i < 1; i++) {
-        const firstCountry = prompt("Which country did you visit first?");
-        const rateCountry = +prompt(`How would you rate your visit to ${firstCountry}?`);
-        const likeCountry = prompt("Which country do you like?");
-    
-    if (firstCountry != null && likeCountry != null && rateCountry != null && firstCountry != "" && likeCountry != "" && rateCountry != "" && firstCountry.length < 30) {
-        obj.continent[firstCountry] = rateCountry;
-    console.log("Done");
-        } else {
-            console.log("Canceled");
-            i--;
+    private: false,
+    init() {
+        this.count = +prompt("How many countries have you visited?");
+
+        while (this.count == "" || this.count == null || isNaN(this.count)) {
+            this.count = +prompt("How many countries have you visited?");
         }
-    console.log(obj);
-    }
-  }
-function detectUserPersonalLevel() {
-    if (obj.count < 2){
-        obj.status = "You aren't travel lover";
-        } else if 
-            (obj.count >= 10) {
-            obj.status = "You are travel lover"
+
+    },
+    rememberAndCreateUserCountry() {
+        for (let i = 0; i < 1; i++) {
+            const firstCountry = prompt("Which country did you visit first?");
+            const rateCountry = +prompt(`How would you rate your visit to ${firstCountry}?`);
+            const likeCountry = prompt("Which country do you like?");
+
+            if (firstCountry != null && likeCountry != null && rateCountry != null && firstCountry != "" && likeCountry != "" && rateCountry != "" && firstCountry.length < 30) {
+                this.continent[firstCountry] = rateCountry;
+                console.log("Done");
             } else {
-                console.log("It happened problem ");
-            }  
-}
+                console.log("Canceled");
+                i--;
+            }
+            console.log(obj);
+        }
+    },
+    detectUserPersonalLevel() {
+        if (this.count < 2) {
+            this.status = "You aren't travel lover";
+        } else if
+            (this.count >= 10) {
+            this.status = "You are travel lover"
+        } else {
+            console.log("It happened problem ");
+        }
+    },
+    checkObjPrivateStatus(state) {
+        state ? console.log("Sorry but obj is not visible") : console.log(obj);
 
+    },
+    userReligion() {
+        for (let i = 0; i < 2; i++) {
+            this.religion.push(prompt(`What religion do you belong to ${i + 1}?`))
 
-function checkObjPrivateStatus(state) {
-    state ? console.log("Sorry but obj is not visible"): console.log(obj);
-    
-}
+        }
+    },
+    startApp() {
+        this.init()
 
-function userReligion() {
-    for (let i = 0; i < 2; i++) {
-    obj.religion.push(prompt(`What religion do you belong to? ${i + 1}`))
-        
+        this.rememberAndCreateUserCountry();
+        this.userReligion();
+        this.detectUserPersonalLevel();
+        this.checkObjPrivateStatus(this.private);
     }
-}
-rememberAndCreateUserCountry();
-userReligion()
-detectUserPersonalLevel();
-checkObjPrivateStatus(obj.private);
+};
+app.startApp();
 
